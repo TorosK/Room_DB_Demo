be79db34cf78e2db47743b303e73d94d18bb7378
Toros Kutlu <toros_kutlu@hotmail.com>
2022-11-28 15:08:11 +0100

    Need to work on front end

--- app/src/main/java/com/example/room_db_demo/AppDataBase.kt
+++ app/src/main/java/com/example/room_db_demo/AppDataBase.kt
@@ -1,4 +1,31 @@
 package com.example.room_db_demo
 
-class AppDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Reminder :: class], version = 1)
+abstract class AppDataBase : RoomDatabase() {
+    abstract fun reminderDao() : ReminderDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE : AppDataBase? = null
+        fun getDatabase(context : Context): AppDataBase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDataBase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
